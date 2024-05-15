f9dac04d63a9e40da1eecd410aefb77f09dd54d1
ldirocco <83399603+ldirocco@users.noreply.github.com>
2024-05-15 11:02:29 +0200

    Update processes.py

--- src/processes.py
+++ src/processes.py
@@ -1,52 +1,54 @@
 import edlib
+import pyabpoa as pa
 
-def process_target(target, target_sequence, overlapping_df, len_window, fastq_df):
-    len_target = len(target_sequence)
-    n_windows = len_target // len_window
-    if len_target % len_window != 0:
-        n_windows += 1
-    windows = {}
-    for i in range(0, n_windows):
-        windows[f"{target}__{i}"]=[target_sequence[(i * len_window): ((i + 1) * len_window)]]
+def process_target(target, t_seq, overlapping_df, len_window, fastq_df):
+    l=len(t_seq)
+    overlapping_df=paf_df[paf_df["q_seq_name"] == target]
+    n_windows=l//wLength
+    if l%wLength!=0:
+        n_windows+=1
+    windows={}
+    prev_index=0
+    for i in range(0,n_windows):
+        windows[i]=[t_seq[prev_index:min((i+1)*wLength,l)]]
+        prev_index=min((i+1)*wLength,l)
     
     for index, row in overlapping_df.iterrows():
         windows_borders=[]
-        r=int(row['q_start'])
-        windows_borders=[(0,r//len_window)]
-        query_sequence=fastq_df[row['t_seq_name']]
-        q_start=int(row['t_start'])
-        q_end=int(row['t_end'])
-        query_sequence=query_sequence[q_start:q_end]
+        t_start=row['q_start']
+        t_end=row['q_end']
+        q_start=row['t_start']
+        q_end=row['t_end']
+        windows_borders=[(-1,t_start//wLength)]
+        query_sequence=fastq_df[fastq_df['seq_id']==row['t_seq_name']]['sequence'].iloc[0]
+        query_sequence=query_sequence[q_start:q_end+1]
+        target_sequence=t_seq[t_start:t_end+1]
         edit_distance = edlib.align(query=query_sequence, target=target_sequence, mode="NW", task="path")
         niceNW = edlib.getNiceAlignment(edit_distance, query=query_sequence, target=target_sequence)
-        target_alignment=niceNW["target_aligned"]
-        query_alignment=niceNW["query_aligned"]
-        
-        #print(target_alignment)
-        #print(query_alignment)
-        
+        target_alignment=niceNW['target_aligned']
+        query_alignment=niceNW['query_aligned']
         w_id=0
         for i in range(0,len(target_alignment)):
             if target_alignment[i]!='-':
-                r+=1
-                if r%len_window==0:
-                    w_id=r/len_window
+                t_start+=1
+                if t_start%wLength==0:
+                    w_id=t_start/wLength
                     windows_borders.append((i,w_id))
         w_id+=1
         windows_borders.append((len(query_alignment),w_id))
-        #w=windows_borders[0]
-
-        #for i in range(1,len(windows_borders)-1):
-        #    w_next=windows_borders[i]
-        #    windows[w[1]].append(query_alignment[w[0]:w_next[0]].replace("-",""))
-        #    w=w_next
-        #windows[w[1]].append(query_alignment[int(len_window*w[1]+1):w[0]].replace("-","")) 
-        #print(windows_borders)
+        w=windows_borders[0]
+        for i in range(1,len(windows_borders)):
+            if w[1]>=n_windows:
+                    break
+             w_next=windows_borders[i]
     
     return windows
 
 
 def process_window(key, window):
-    # TODO
-    graph = {}
-    return graph
+    consensus_sequence=''
+    for i in range(0,len(windows)):
+        w_consensus=a.msa(windows[i], out_cons=True, out_msa=True)
+        consensus_sequence+=w_consensus.cons_seq[0]
+    
+    return consensus_sequence
